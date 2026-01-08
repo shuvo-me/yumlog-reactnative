ed9b47f88956162d6bed30e884caeee33028c0d5
shuvo-me <shuvo.js.dev@gmail.com>
2026-01-08 11:35:20 +0600

    create: auth services

---
+++ services/auth.service.ts
@@ -0,0 +1,32 @@
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    updateProfile
+} from "firebase/auth";
+import { auth } from "../lib/firebase";
+
+// Register a new user
+export const registerUser = async (email: string, password:string) => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    // Optionally add the user's name to their profile
+    await updateProfile(userCredential.user,{displayName: 'User'});
+    return userCredential.user;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+};
+
+// Login existing user
+export const loginUser = async (email: string, password: string) => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    return userCredential.user;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+};
+
+// Logout
+export const logoutUser = () => signOut(auth);
